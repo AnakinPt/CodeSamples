fc4c40ea7715a5483cf5ab2edec08ce426d81531
Hugo Dias <hugo@hugo-dias.com.pt>
2016-02-15 00:27:24 +0000

    Add jdbc to my scenario

---
+++ cache/src/main/java/pt/com/hugodias/cache/app/App.java
@@ -0,0 +1,39 @@
+package pt.com.hugodias.cache.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.util.StopWatch;
+
+import pt.com.hugodias.cache.config.AppConfig;
+import pt.com.hugodias.cache.dao.UserDaoService;
+
+public class App {
+	private static final Logger log = LoggerFactory.getLogger(App.class);
+	
+	public static void main(String[] args) {
+	     
+	    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+	    UserDaoService obj = (UserDaoService) context.getBean("userDao");
+
+        StopWatch stopwatch = new StopWatch();
+        for (int i = 0; i< 200 ; i++){
+            stopwatch.start(String.valueOf(i));
+            long userId = 500 + i % 10;
+	    	log.debug("Result : {}", obj.findById(userId));
+            stopwatch.stop();
+            try {
+				Thread.sleep(1000L);
+			} catch (InterruptedException e) {
+				// do nothing
+			}
+        }
+        System.out.println(stopwatch.prettyPrint());
+
+	  //shut down the Spring context.
+	    ((ConfigurableApplicationContext)context).close();
+	    	    
+	}
+}
---
+++ cache/src/main/java/pt/com/hugodias/cache/config/AppConfig.java
@@ -0,0 +1,10 @@
+package pt.com.hugodias.cache.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({ "pt.com.hugodias.cache.dao" })
+public class AppConfig {
+	
+}
